4963c04c25c9dcd0ac6986743df934d100f99a6d
Diya Patel <diya427.p@gmail.com>
2026-02-24 13:02:42 -0500

    Able to upload one image and move it around

--- Scrapbook/ContentView.swift
+++ Scrapbook/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
@@ -17,6 +18,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
+                        ItemDetailView(item: item)
                     } label: {
                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, ))
                     }
@@ -34,7 +36,11 @@ struct ContentView: View {
                 }
             }
         } detail: {
-            Text("Select an item")
+            if let selectedItem = items.first {
+                ItemDetailView(item: selectedItem)
+            } else {
+                Text("Select an item")
+            }
         }
     }
 
@@ -49,6 +55,58 @@ struct ContentView: View {
         withAnimation {
             for index in offsets {
                 modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+struct ItemDetailView: View {
+    @Bindable var item: Item
+    @State private var photoSelection: PhotosPickerItem?
+    @State private var dragOffset: CGSize = .zero // tracks movement of finger
+    
+    var body: some View{
+        VStack {
+            // check if the item has image data and to be converted to UIimage
+            if let imageData = item.imageData, let uiImage = UIImage(data: imageData) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .offset(x: item.offSetX + dragOffset.width, y: item.offSetY + dragOffset.height)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                // updates postions as your finger moves
+                                dragOffset = value.translation
+                            }
+                            .onEnded{ value in
+                                // save position once finger is lifted
+                                item.offSetX += value.translation.width
+                                item.offSetY += value.translation.height
+                                
+                                dragOffset = .zero
+                            }
+                    )
+            } else {
+                // show when theres no photo
+                ContentUnavailableView("No added image", systemImage: "photo.badge.minus")
+                    
+            }
+        }
+        .navigationTitle(item.timestamp.formatted(date: .numeric, time: .omitted))
+        // button for top
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                PhotosPicker(selection: $photoSelection, matching: .images) {
+                    Image(systemName: "photo.badge.plus")
+                }
+            }
+        }
+        .onChange(of: photoSelection) {oldValue,newValue in
+            Task {
+                if let data = try? await newValue?.loadTransferable(type: Data.self) {
+                    item.imageData = data
+                }
             }
         }
     }
--- Scrapbook/Item.swift
+++ Scrapbook/Item.swift
@@ -12,7 +12,17 @@ import SwiftData
 final class Item {
     var timestamp: Date
     
-    init(timestamp: Date) {
+    // store the photo
+    @Attribute(.externalStorage) var imageData: Data?
+    
+    // track postion of image
+    var offSetX: Double = 0.0
+    var offSetY: Double = 0.0
+    
+    init(timestamp: Date, imageData: Data?=nil, offSetX: Double=0.0, offSetY: Double=0.0) {
         self.timestamp = timestamp
+        self.imageData = imageData
+        self.offSetX = offSetX
+        self.offSetY = offSetY
     }
 }
